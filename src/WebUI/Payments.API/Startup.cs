afc773347f928c69013a156dcf0dfc342a6037d0
Vinay Arora <vinay.arora7171@hotmail.com>
2021-02-21 21:32:04 +0530

    Setup API versioning & swagger

--- src/WebUI/Payments.API/Startup.cs
+++ src/WebUI/Payments.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Payments.Application;
 using Payments.Infrastructure;
 using System;
@@ -30,6 +31,31 @@ namespace Payments.API
             services.AddApplication()
                     .AddInfrastructure(Configuration)
                     .AddControllers();
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\Payments.API.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Payments.API",
+                });
+            });
+            #endregion
+
+            #region API Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +71,17 @@ namespace Payments.API
             }
 
             app.UseHttpsRedirection();
+
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Payments.API");
+            });
+            #endregion
 
             app.UseRouting();
 
