55f9dbb8dff0bca531ac23926a71f839bf61e8e9
Vinay Arora <vinay.arora7171@hotmail.com>
2021-02-21 19:20:46 +0530

    Setup the Solution Structure

---
+++ src/Domain/Payments.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Payments.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Infrastructure/Payments.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Payments.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
